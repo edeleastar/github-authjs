6aadbb818e40798aa97dcf98577ba2d8681c343b
Eamonn de Leastar <edeleastar@wit.ie>
2024-10-26 10:43:25 +0100

    added basepath

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,5 @@
 export const load = async ({ locals }) => {
-  // const session = await locals.getSession();
+  const session = await locals.getSession();
   const loggedIn = false; //!!session?.user;
 
   const user = null; //session?.user;
